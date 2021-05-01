15d03c58fc2748d9df2feb9b2fe78bde5f3598c7
sirius93 <nandan.1345@gmail.com>
2021-05-01 17:03:44 +0530

    initial website content

--- HTML/php/mail.php
+++ HTML/php/mail.php
@@ -3,7 +3,7 @@
 /* =====================================================
  * change this to the email you want the form to send to
  * ===================================================== */
-$email_to = "you@company.pw"; 
+$email_to = "aakanksha@storified.me"; 
 $email_from = "webmaster@company.pw"; // must be different than $email_from 
 $email_subject = "Contact Form submitted";
 
